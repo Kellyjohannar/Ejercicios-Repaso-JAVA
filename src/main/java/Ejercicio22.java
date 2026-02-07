f3327299a7e8630d635c631d9753dadf164e6927
Kelly Johanna <keljohagu@gmail.com>
2026-02-07 21:02:29 +0100

    Commit

--- src/main/java/Ejercicio22.java
+++ src/main/java/Ejercicio22.java
@@ -1,4 +1,29 @@
 //Función que invierta arrays y devuelva uno nuevo.
 public class Ejercicio22 {
-    
+
+    public static int[] invertirArray(int[] arrayOriginal) {
+        int[] arrayInvertido = null;
+
+        if (arrayOriginal != null) {
+            int longitud = arrayOriginal.length;
+            arrayInvertido = new int[longitud];
+
+            for (int i = longitud - 1; i >= 0; i--) {
+                arrayInvertido[(longitud - 1) - i] = arrayOriginal[i];
+            }
+        }
+
+        return arrayInvertido;
+    }
+
+    public static void main(String[] args) {
+        int[] entrada = {1, 2, 3, 4, 5};
+        int[] resultado = invertirArray(entrada);
+
+        if (resultado != null) {
+            for (int num : resultado) {
+                System.out.print(num + " ");
+            }
+        }
+    }
 }
