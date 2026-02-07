6f162c148fcc808a7aa5e139edf2fc46728a59d1
Kelly Johanna <keljohagu@gmail.com>
2026-02-07 21:11:33 +0100

    Commit

--- src/main/java/Ejercicio21.java
+++ src/main/java/Ejercicio21.java
@@ -1,4 +1,28 @@
 //Función esPrimo() y usarla para mostrar primos del 1 al 100.
 public class Ejercicio21 {
-    
+    public static boolean esPrimo(int numero) {
+        boolean esPrimo = true;
+
+        if (numero <= 1) {
+            esPrimo = false;
+        } else {
+            for (int i = 2; i <= numero / 2 && esPrimo; i++) {
+                if (numero % i == 0) {
+                    esPrimo = false;
+                }
+            }
+        }
+
+        return esPrimo;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Números primos del 1 al 100:");
+
+        for (int i = 1; i <= 100; i++) {
+            if (esPrimo(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
--- src/main/java/Ejercicio24.java
+++ src/main/java/Ejercicio24.java
@@ -1,4 +1,36 @@
 //Generador de contraseñas aleatorias con minúsculas, mayúsculas y números.
+import java.util.Random;
+
 public class Ejercicio24 {
-    
+    public static String generarPassword(int longitud) {
+        String passwordResultado = "";
+
+        if (longitud > 0) {
+            String minusculas = "abcdefghijklmnopqrstuvwxyz";
+            String mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String numeros = "0123456789";
+            
+            String caracteres = minusculas + mayusculas + numeros;
+            
+            char[] passwordArray = new char[longitud];
+            Random random = new Random();
+
+            for (int i = 0; i < longitud; i++) {
+                int indiceAleatorio = random.nextInt(caracteres.length());
+                
+                passwordArray[i] = caracteres.charAt(indiceAleatorio);
+            }
+
+            passwordResultado = new String(passwordArray);
+        }
+
+        return passwordResultado;
+    }
+
+    public static void main(String[] args) {
+        int longitudPassword = 10;
+        String nuevaPassword = generarPassword(longitudPassword);
+        
+        System.out.println("Contraseña generada: " + nuevaPassword);
+    }
 }
